7b80713e1380827db6bcb33587be97da12cdc8ab
root <you@example.com>
2020-02-26 16:35:47 +0530

    test

--- product_supplier_history/__manifest__.py
+++ product_supplier_history/__manifest__.py
@@ -14,7 +14,7 @@ This module have new customization of New Car Repair Service .
         'security/ir.model.access.csv',
         'views/product_history_view.xml',
         'views/currency_rates_view.xml',
-        'views/supplier_history_view.xml',
+        # 'views/supplier_history_view.xml',
     ],
     'demo': [
         # 'data/sale_demo.xml',
