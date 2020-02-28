396124bed466aafc507478eebb48d6a7e0acd75a
root <you@example.com>
2020-02-28 10:35:30 +0530

    changes

--- product_supplier_history/__manifest__.py
+++ product_supplier_history/__manifest__.py
@@ -11,7 +11,7 @@ This module have new customization of New Car Repair Service .
     'depends': ['base', 'stock', 'sale', 'product', 'mail', 'purchase', 'fleet'],
     'data': [
         'security/ir.model.access.csv',
-        'views/product_history_view.xml',
+        # 'views/product_history_view.xml',
         'views/currency_rates_view.xml',
         'views/supplier_history_view.xml',
         'views/sale_order_view.xml',
